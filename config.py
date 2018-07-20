9a9ac4b56098b2008ed113889ac8fb481e8df8c0
Hunter Johnson <hhjohnso@indiana.edu>
2018-07-20 00:43:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_CLS_PATH = os.path.join(base_dir, 'model.py')
-MODEL_CLS_NAME = 'ViewModality'
+MODEL_CLS_NAME = 'Model_Name'
 MODEL_LOAD_ARGS = {
     'data_dir': os.path.join(base_dir, 'data-vol'),
 }
